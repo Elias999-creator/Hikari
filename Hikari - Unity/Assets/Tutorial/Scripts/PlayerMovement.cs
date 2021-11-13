2dff53bf706cda988c634d2204812ea70f9b0a03
user <71507603+Elias999-creator@users.noreply.github.com>
2021-11-13 00:28:59 +0000

    Create run animation

--- Hikari - Unity/Assets/Tutorial/Scripts/PlayerMovement.cs
+++ Hikari - Unity/Assets/Tutorial/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2D;
     private Animator animator;
 
+    private bool facingRight = true;
+
     public float speed = 2.0f;
     public float horizMovement;
 
@@ -29,5 +31,20 @@ public class PlayerMovement : MonoBehaviour
     {
         // move the charachyer left and right
         rb2D.velocity = new Vector2(horizMovement*speed, rb2D.velocity.y);
+        animator.SetFloat("speed", Mathf.Abs(horizMovement));
+        Flip(horizMovement);
     }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal < 0 && facingRight || horizontal > 0 && !facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
 }
