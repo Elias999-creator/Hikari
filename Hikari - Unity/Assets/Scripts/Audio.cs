883e80368de178ff5c79b0e29e2b06d5a667f008
Elias Preston <511259@escg.ac.uk>
2021-12-03 11:30:05 +0000

    Add placeholder music

---
+++ Hikari - Unity/Assets/Scripts/Audio.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    private static Audio instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+}
