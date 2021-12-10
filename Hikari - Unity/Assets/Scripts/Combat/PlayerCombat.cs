8e2c3504686fbc8424d90247d84eca056ccd7bd6
Elias Preston <511259@escg.ac.uk>
2021-12-10 15:27:43 +0000

    At 0 HP reload active scene

--- Hikari - Unity/Assets/Scripts/Combat/PlayerCombat.cs
+++ Hikari - Unity/Assets/Scripts/Combat/PlayerCombat.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCombat : MonoBehaviour
 {
@@ -88,5 +89,6 @@ public class PlayerCombat : MonoBehaviour
         GetComponent<Collider2D>().enabled = false;
         GetComponent<PlayerMovement>().enabled = false;
         this.enabled = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
