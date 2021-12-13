0a64f36b8b646913ab7e0203c512b073e30dab7f
user <71507603+Elias999-creator@users.noreply.github.com>
2021-12-13 00:44:06 +0000

    Add parralax to swamp

--- Hikari - Unity/Assets/Scripts/Parralax.cs
+++ Hikari - Unity/Assets/Scripts/Parralax.cs
@@ -24,8 +24,5 @@ public class Parralax : MonoBehaviour
         float dist = (cam.transform.position.x * parallaxEffect);
 
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
-
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
     }
 }
